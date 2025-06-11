3f0cb2142e04fb00810f8500fb260619af9ac95b
DiegoMarkzs <119617704+DiegoMarkzs@users.noreply.github.com>
2025-06-10 22:06:53 -0300

    Criada classe denuncia DAO

---
+++ src/main/java/model/Denuncia.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.sql.Date;
+
+public class Denuncia {
+
+    private int id;
+
+    private Date data;
+
+    private String Assunto;
+
+
+    public int GetID(){
+        return id;
+        
+    }
+
+    
+
+}
